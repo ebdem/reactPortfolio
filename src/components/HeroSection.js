1788694196030ba66b3d65bc6e2a8122c7f61ea0
ebdem <ebu.bekir.3333@gmail.com>
2020-09-29 14:02:36 +0300

    new pages added and rewrited styles

---
+++ src/components/HeroSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "../components/Button";
+import "./HeroSection.css";
+import "../App.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src="/videos/video1.mp4" autoPlay loop muted />
+      <h1>DEMIRAY YAZILIM</h1>
+      <p>What are you looking for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          LOOK TRAILER <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
